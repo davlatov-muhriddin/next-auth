0eefb53aebebea1a6d7d8db192e70e089b299655
davlatov-muhriddin1 <muhriddind819@gmail.com>
2024-03-28 19:17:46 +0500

    add provider url

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { AuthProvider } from "./provider";
+import { AuthProvider } from "./Provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
