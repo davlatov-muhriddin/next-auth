8db79c2e5d22c3544af1579f7d3683f46e529656
davlatov-muhriddin1 <muhriddind819@gmail.com>
2024-03-28 19:22:30 +0500

    change '

--- components/LoginForm.jsx
+++ components/LoginForm.jsx
@@ -57,7 +57,7 @@ function LoginForm() {
           )}
 
           <Link className="text-sm mt-3 text-right" href={"/register"}>
-            Don't have an account <span className="underline">Register</span>
+            Dont have an account <span className="underline">Register</span>
           </Link>
         </form>
       </div>
